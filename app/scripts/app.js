174ff3eeb68245c06476d9c912c74e9bbb1d1f1c
Vadim Solovei <chillout05@gmail.com>
2017-01-12 17:08:26 +0200

    add slider js

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,22 @@
 import svg4everybody from 'svg4everybody';
 import $ from 'jquery';
+import './jquery-ui.js';
 
 $(() => {
 	svg4everybody();
+});
+
+const sliderValue = [1, 4, 8, 16];
+
+$(function () {
+	$( '.slider__range' ).slider({
+		value: 4,
+		min: 1,
+		max: 16,
+		step: 1,
+		animate: 300,
+		slide: function ( event, ui ) {
+			if ( sliderValue.indexOf(ui.value) === -1 )return false;
+		}
+	});
 });
