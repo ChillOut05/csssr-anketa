3d4c77e9ba1553c247a6783226d11b9db9ee887d
Vadim Solovei <chillout05@gmail.com>
2017-01-30 16:53:05 +0200

    add desktop name mod for slider

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -7,24 +7,26 @@ $(() => {
 	svg4everybody();
 });
 
-// slider
+// slider desktop
 const sliderValue = [1, 4, 8, 16];
-const sliderValueMobile = [1, 9, 13, 16];
 
 $(function () {
-	$( '.slider__range' ).slider({
+	$( '.slider__range_desktop' ).slider({
 		value: 4,
 		min: 1,
 		max: 16,
 		step: 1,
 		animate: 300,
-		slide: function ( event, ui ) {
+		slide: ( event, ui ) => {
 			if ( sliderValue.indexOf(ui.value) === -1 ) {
 				return false;
 			}
 		}
 	});
 });
+
+// slider mobile
+const sliderValueMobile = [1, 9, 13, 16];
 
 $(function () {
 	$( '.slider__range_mobile' ).slider({
@@ -34,7 +36,7 @@ $(function () {
 		max: 16,
 		step: 1,
 		animate: 300,
-		slide: function ( event, ui ) {
+		slide: ( event, ui ) => {
 			if ( sliderValueMobile.indexOf(ui.value) === -1 ) {
 				return false;
 			}
